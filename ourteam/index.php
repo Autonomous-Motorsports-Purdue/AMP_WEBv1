1350d01a7592c563b054cbd74ffb9be7b82710c9
Sam Bhatt <sambhatt000@gmail.com>
2024-02-27 14:28:13 -0500

    Update ourteam
    Added Footer

--- ourteam/index.php
+++ ourteam/index.php
@@ -1,16 +1,56 @@
+<!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Page Under Construction- AMP</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Autonomous Motorsports Purdue (AMP) is an organization focused on educating students about autonomous driving and inspiring innovation through student-led projects.">
+		<title>Autonomous Motorsports Purdue</title>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/AMP/public_html/assets/header.php'; ?>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="/AMP/assets/style.css">
 	</head>
 	<body>
-		<center>
-			<h1>Page Under Construction</h1>
-			<hr>
-			<p>Please check back later.</p>
-			<span style="font-size: 144px;">&#x26D4;</span>
-			<br>
-			<br>
-			<a href="/AMP/test.php">Go Back.</a>
-		</center>
+		<nav>
+			<a class="nav-home" href="/AMP">
+				<img class="nav-logo" src="/AMP/images/amp.png">
+			</a>
+			<div class="nav-container">
+				<a class="nav-links" href="/AMP" >Home</a>
+				<a class="nav-links" href="/AMP/ourteam" >Our Team</a>
+				<a class="nav-links" href="/AMP/projects" >Projects</a>
+				<a class="nav-links" href="/AMP/news" >News</a>
+				<a class="nav-links" href="/AMP/sponsors" >Sponsors</a>
+				<a class="nav-links" href="/amp/contact" >Contact</a>
+			</div>
+		</nav>
+		<div class="container">
+			<h2>Who We Are</h2>
+			<div class="box-left">
+				<img class="sub-img" src="images/kart.jpg" alt="Go kart on starting line.">
+			</div>
+			<div class="box-right">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+					labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies mi eget. Tellus elementum 
+					sagittis vitae et leo. Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies. Donec pretium 
+					vulputate sapien nec sagittis. Facilisi cras fermentum odio eu feugiat pretium. Aenean sed adipiscing diam 
+					donec adipiscing tristique. Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean. Facilisis 
+					mauris sit amet massa vitae tortor condimentum.
+				<br><br>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+					labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies mi eget. Tellus elementum 
+					sagittis vitae et leo. Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies. Donec pretium 
+					vulputate sapien nec sagittis. Facilisi cras fermentum odio eu feugiat pretium. Aenean sed adipiscing diam 
+					donec adipiscing tristique. Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean. Facilisis 
+					mauris sit amet massa vitae tortor condimentum.
+				</p>
+			</div>
+		</div>
+		
+		<div class="container">
+			
+		</div>
+		<br>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/AMP/public_html/assets/footer.php'; ?>
+
 	</body>
 </html>
