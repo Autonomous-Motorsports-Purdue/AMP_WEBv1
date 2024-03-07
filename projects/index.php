8600f18f23103a9be1864dd793b1c9596b75c65e
Sam Bhatt <sambhatt000@gmail.com>
2024-03-07 14:46:59 -0500

    Update projects
    Added content & general structure to page.

--- projects/index.php
+++ projects/index.php
@@ -1,16 +1,106 @@
+<!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Page Under Construction- AMP</title>
-	</head>
-	<body>
-		<center>
-			<h1>Page Under Construction</h1>
-			<hr>
-			<p>Please check back later.</p>
-			<span style="font-size: 144px;">&#x26D4;</span>
-			<br>
-			<br>
-			<a href="/AMP/test.php">Go Back.</a>
-		</center>
-	</body>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Autonomous Motorsports Purdue (AMP) is an organization focused on educating students about autonomous driving and inspiring innovation through student-led projects.">
+    <title>Projects | Autonomous Motorsports Purdue</title>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/AMP/public_html/assets/header.php'; ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+
+<body>
+    <nav>
+        <a class="nav-home" href="/AMP">
+            <img class="nav-logo" src="/AMP/images/amp.png" />
+        </a>
+        <div class="nav-container" id="mobile-container">
+            <a class="nav-links" href="/AMP">Home</a>
+            <a class="nav-links" href="/AMP/ourteam">Our Team</a>
+            <a class="nav-links" href="/AMP/projects">Projects</a>
+            <a class="nav-links" href="/AMP/news">News</a>
+            <a class="nav-links" href="/AMP/sponsors">Sponsors</a>
+            <a class="nav-links" href="/amp/contact">Contact</a>
+        </div>
+        <a href="javascript:void(0);" class="mobile-icon" onclick="mobileNav()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </nav>
+   <div class="container-banner">
+		<h1>Projects</h1>
+		<img id="banner" src="/AMP/images/banner.jpg" alt="Team Workshop">
+	</div>
+	
+	<div class="container-header">
+		<h2>Software</h2>
+	</div>
+	<div class="container">
+		<img class="sub-img" src="/AMP/images/kart.jpg" alt="Go kart on starting line">
+		<p>
+			Autonomous racing is the ultimate engineering challenge. Combining the mechanical design of
+			traditional auto-racing with the cutting-edge software controls of driverless vehicles,
+			autonomous go-karts are the perfect platform to design, build, and test the vehicles of tomorrow.
+			<br><br>
+			The driver is replaced with a variety of sensors placed throughout the kart. These
+			sensors act as the eyes and ears of the vehicle, feeding the data to a control algorithm
+			that students develop and tune. Machine learning (artificial intelligence) is often used to
+			interpret sensor data. The challenge is who can engineer and build the most effective and fast driverless kart.
+		</p>
+    </div>
+	<div class="container-header">
+        <h2>Electrical</h2>
+	</div>
+	<div class="container">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies mi eget. Aenean sed adipiscing diam
+			donec adipiscing tristique. Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean. Facilisis
+			mauris sit amet massa vitae tortor condimentum.
+			<br><br>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies mi eget. Tellus elementum
+			sagittis vitae et leo. Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies. Donec pretium
+			vulputate sapien nec sagittis. Facilisi cras fermentum odio eu feugiat pretium.
+		</p>
+		<img class="sub-img" src="/AMP/images/aks.jpg" alt="Autonomous Karting Series team picture.">
+    </div>
+	<div class="container-header">
+		<h2>Mechancial</h2>
+	</div>
+	<div class="container">
+		<img class="sub-img" src="/AMP/images/kart.jpg" alt="Go kart on starting line">
+		<p>
+			Autonomous racing is the ultimate engineering challenge. Combining the mechanical design of
+			traditional auto-racing with the cutting-edge software controls of driverless vehicles,
+			autonomous go-karts are the perfect platform to design, build, and test the vehicles of tomorrow.
+			<br><br>
+			The driver is replaced with a variety of sensors placed throughout the kart. These
+			sensors act as the eyes and ears of the vehicle, feeding the data to a control algorithm
+			that students develop and tune. Machine learning (artificial intelligence) is often used to
+			interpret sensor data. The challenge is who can engineer and build the most effective and fast driverless kart.
+		</p>
+    </div>
+	<div class="container-header">
+        <h2>Business</h2>
+	</div>
+	<div class="container">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies mi eget. Aenean sed adipiscing diam
+			donec adipiscing tristique. Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean. Facilisis
+			mauris sit amet massa vitae tortor condimentum.
+			<br><br>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Amet aliquam id diam maecenas ultricies mi eget. Tellus elementum
+			sagittis vitae et leo. Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies. Donec pretium
+			vulputate sapien nec sagittis. Facilisi cras fermentum odio eu feugiat pretium.
+		</p>
+		<img class="sub-img" src="/AMP/images/aks.jpg" alt="Autonomous Karting Series team picture.">
+    </div>
+    <br>
+	<br>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/AMP/public_html/assets/footer.php'; ?>
+</body>
+
 </html>
