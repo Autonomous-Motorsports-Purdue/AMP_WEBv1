8908bc0826eb71390b852816785270e67a165a2f
Sam Bhatt <sambhatt000@gmail.com>
2024-08-27 17:51:53 -0400

    Update header.php

--- assets/header.php
+++ assets/header.php
@@ -1,3 +1,13 @@
 <?php
-	
+	echo "
+<!-- Google tag (gtag.js) -->
+<script async src='https://www.googletagmanager.com/gtag/js?id=G-MHZ3TSS63T'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MHZ3TSS63T');
+</script>
+";
 ?>
